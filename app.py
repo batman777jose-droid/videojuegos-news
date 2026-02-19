4bc25daf9e32fc357d5567cd6bc19e58d595f56c
Cristo Fernando <pastenesfernando52@gmail.com.com>
2026-02-18 21:26:14 -0600

    Avance Fernando y Jose

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
+@app.route('/noticias')
+def noticias():
+    return render_template('noticias.html')
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
